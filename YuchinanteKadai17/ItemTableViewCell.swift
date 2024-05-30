3a332f778ed0a85dbdd4838cb7c3f7366c3488f2
yuya-3110 <ysuu45143745@gmail.com>
2024-05-31 02:22:58 +0900

    課題16完了時の状態

---
+++ YuchinanteKadai17/ItemTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ItemTableViewCell.swift
+//  YuchinanteKadai17
+//  
+//  Created by Yuchinante on 2024/05/24
+//  
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    // アイテム名が選択されているかを表示する画像ビュー
+    @IBOutlet weak var checkImageView: UIImageView!
+
+    // アイテム名を表示するラベル
+    @IBOutlet weak var nameLabel: UILabel!
+
+    // セルの表示を設定するメソッド
+    func configure(name: String, isChecked: Bool) {
+        if isChecked {
+            checkImageView.image = UIImage(named: "check")  // チェックマークの画像を表示
+        } else {
+            checkImageView.image = nil  // チェックマークの画像を非表示
+        }
+
+        // ラベルにアイテム名を表示
+        nameLabel.text = name
+    }
+}
